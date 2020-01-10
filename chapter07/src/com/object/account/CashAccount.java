2629e8dd1daa1d525d0ac1f4af0b1162ffc1db40
aqi00 <aqi00@163.com>
2020-01-10 14:43:14 +0800

    第7章 类的三要素

--- chapter07/src/com/object/account/CashAccount.java
+++ chapter07/src/com/object/account/CashAccount.java
@@ -3,12 +3,12 @@ package com.object.account;
 // 定义一个现金账户类
 public class CashAccount extends Account {
 	public final static int RMB = 0; // 人民币
-	public final static int HKD = 1; // 港币
+	public final static int SGD = 1; // 新加坡元
 	public final static int USD = 2; // 美元
 	public final static int EUR = 3; // 欧元
 	public final static int GBP = 4; // 英镑
 	public final static int JPY = 5; // 日元
-	public final static String[] typeNames = new String[]{"人民币","港币","美元","欧元","英镑","日元"};
+	public final static String[] typeNames = new String[]{"人民币","新加坡元","美元","欧元","英镑","日元"};
 	private int cashType; // 现金类型
 
 	// 现金账户的构造方法
